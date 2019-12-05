8640d564ef36a3815a74b11ac469c1427f79db7d
bbohling <brandon@bohling.me>
2019-12-04 21:21:08 -0800

    feat: switch to oauth

--- api/helpers/strava.js
+++ api/helpers/strava.js
@@ -1,5 +1,4 @@
-
-const Promise = require("bluebird");
+const Promise = require('bluebird');
 const request = require("request-promise");
 const moment = require("moment");
 
@@ -22,24 +21,95 @@ module.exports = {
   fn: startIngest
 }
 
+const defaultUser = 'brandon';
 let ingestAllData = false;
+let tokenInfo = {};
+let user;
 
 async function startIngest(inputs, exits) {
   ingestAllData = inputs.getAll;
-  const userToken = sails.config.users[inputs.userId].accessToken;
-  const options = requestOptions(userToken);
-  const done = await getActivities(options);
-  return exits.success('success');
+  user = inputs.userId || defaultUser;
+  // const userToken = sails.config.users[inputs.userId].accessToken;
+  const tokens = await requestRefreshToken(user);
+  if (tokens) {
+    const options = requestOptions(tokens);
+    console.log(options);
+    const done = await getActivities(options);
+    return exits.success('success');
+  } else {
+    return exits.error({ message: 'error' })
+  }
+
 }
 
-function requestOptions(accessToken) {
+async function requestRefreshToken(user) {
+  // first, check to see if we have a valid token
+  tokenInfo = await User.findOne({ name: user });
+  if (tokenInfo) {
+    // check to see if valid
+    const expires = moment.unix(tokenInfo.expiresAt);
+    // expired token
+    if (moment().isAfter(expires)) {
+      console.log('======= EXPIRED TOKEN ==========');
+      tokenInfo = getNewToken({ user });
+    }
+  } else {
+    // no token,
+    tokenInfo = getNewToken({ user });
+  }
+  console.log(tokenInfo);
+  return tokenInfo;
+}
+
+async function getNewToken({ refreshToken, user }) {
+  const options = {
+    method: 'post',
+    url: `${sails.config.apis.strava.oauth}/token`,
+    qs: {
+      client_id: sails.config.apis.strava.clientId,
+      client_secret: sails.config.apis.strava.clientSecret,
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken || sails.config.users[user].refreshToken
+    }
+  };
+  const response = await request(options);
+  if (response) {
+    const json = JSON.parse(response);
+    // refresh token details
+    const tokenDetails = {
+      name: user,
+      accessToken: json.access_token,
+      refreshToken: json.refresh_token,
+      expiresAt: json.expires_at
+    }
+    console.log(tokenDetails);
+    // see if there is a user
+    const existingUser = await User.findOne({ name: user });
+    if (existingUser) {
+      // update
+      await User.updateOne({ name: user })
+        .set(tokenDetails);
+    } else {
+      // create user
+      await User.create(tokenDetails);
+    }
+    return tokenDetails;
+  } else {
+    console.log(`unable to getNewToken: ${options}`);
+    return null;
+  }
+}
+
+function requestOptions(tokens) {
   // STRAVA API
   return {
     url: sails.config.apis.strava.activities,
     qs: {
-      access_token: accessToken,
       per_page: 200,
       page: 1
+    },
+    headers: {
+      Authorization: `Bearer ${tokens.accessToken}`
     },
     useQuerystring: true
   };
@@ -51,6 +121,7 @@ function getActivities(options) {
     .then(JSON.parse)
     .then(activities => {
       if (activities && activities.length > 0) {
+        // console.log(activities)
         if ((!ingestAllData && options.qs.page < 2) || ingestAllData) {
           options.qs.page++;
           return Promise.map(activities, activity => {
